9b6c244d8555afff27073cc950e7a935953656f1
Fireflow444 <socialmediaframe9@gmail.com>
2026-01-06 14:37:40 -0700

    updated2:37

---
+++ subject-pages/subject-page-files/jscript.js
@@ -0,0 +1,41 @@
+// body size measuring
+const myElement = document.body;
+
+const width = myElement.offsetWidth;
+const height = myElement.offsetHeight;
+
+// Using getBoundingClientRect()
+const rect = myElement.getBoundingClientRect();
+const rectWidth = rect.width;
+const rectHeight = rect.height;
+
+console.log(`Offset Dimensions: ${width}px x ${height}px`);
+console.log(`ClientRect Dimensions: ${rectWidth}px x ${rectHeight}px`);
+
+// responsive redirect
+if (window.innerWidth > 600) {
+  window.location.href = 'https://fireflow444.github.io/Tevta-sallybus-portal/error/faludpage.html';
+}
+
+// home page button
+let hmbtn = document.querySelector(".hm");
+if (hmbtn) {
+  hmbtn.addEventListener("click", () => {
+    window.location.href = 'https://fireflow444.github.io/Tevta-sallybus-portal/Homepage.html';
+  });
+}
+
+// tiktok button
+let tkbtn = document.querySelector(".tk");
+if (tkbtn) {
+  tkbtn.addEventListener("click", () => {
+    let gttk = document.querySelector(".gt-tk");
+    if (gttk) {
+      gttk.style.left = "0px";
+    }
+
+    setTimeout(() => {
+      window.location.href = 'https://www.tiktok.com/@tevta3daecit0';
+    }, 3500);
+  });
+}
